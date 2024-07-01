43ff601338dad80463b4cfd032f0217f558a6ad8
bumgore123 <134861114+bumgore123@users.noreply.github.com>
2024-07-01 21:19:12 +0900

    d

---
+++ new.js
@@ -0,0 +1,8 @@
+const BTN = document.getElementById("BTN");
+const robot = document.getElementById("robot");
+const M = "mc/source/chaemin.jpg";
+function start() {
+    robot.src=M
+    alert("ㅋㅋ병신")
+    
+}
